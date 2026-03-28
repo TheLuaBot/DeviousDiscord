b78ecf5da5661f628a32514dd8a725f923880ce8
Astro <astrochanneloficial@gmail.com>
2026-03-28 13:54:05 -0300

    refactor: Add some """devious""" feature

--- discord/devious/QuickEmbed.py
+++ discord/devious/QuickEmbed.py
@@ -1,5 +1,39 @@
-import discord
+from __future__ import annotations
 
-async def QuickEmbed(self, ctx, title, description, color=0x00ff00):
-    """An Native Method of DeviousDiscord for send embeds!"""
-    embed = discord.Embed(title="Your Title", description="Your Description", color=0x00ff00)
+import datetime
+from typing import Any, Dict, List, Mapping, Optional, Protocol, TYPE_CHECKING, TypeVar, Union
+
+from . import utils
+from .colour import Colour
+from .flags import AttachmentFlags, EmbedFlags
+
+# fmt: off
+__all__ = (
+    'Embed',
+)
+
+class QuickEmbed():
+
+    text: Optional[str]
+    image_url: Optional[str]
+
+    description: str
+    icon_url: Optional[str]
+    
+
+class EmbedProxy:
+    def __init__(self, layer: Dict[str, Any]):
+        self.__dict__.update(layer)
+
+    def __len__(self) -> int:
+        return len(self.__dict__)
+
+    def __repr__(self) -> str:
+        inner = ', '.join((f'{k}={getattr(self, k)!r}' for k in dir(self) if not k.startswith('_')))
+        return f'EmbedProxy({inner})'
+
+    def __getattr__(self, attr: str) -> None:
+        return None
+
+    def __eq__(self, other: object) -> bool:
+        return isinstance(other, EmbedProxy) and self.__dict__ == other.__dict__
